5ea76ae5b258538548a8d0fb3c3dff09031b2a1e
Abhishek Shah <theartofcoding2048@gmail.com>
2024-08-09 16:10:58 -0400

    Support for local motions

--- src-tauri/src/digraph/state.rs
+++ src-tauri/src/digraph/state.rs
@@ -8,11 +8,12 @@ use std::collections::HashMap;
 use thiserror::Error;
 
 static N_ROOT_CHILDREN: phf::Map<&'static str, u8> = phf_map! {
-    "IF" => 2,
-    "IFY" => 1,
-    "IFN" => 1,
-    "WHILE" => 1,
-    "FOR" => 1
+    "CONDTL" => 2,
+    "CONDTLY" => 1,
+    "CONDTLN" => 1,
+    "WHLLOOP" => 1,
+    "FORLOOP" => 1,
+    "FNDEF" => 1,
 };
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -73,6 +74,38 @@ impl CursorDir {
         Ok(())
     }
 
+    fn _find_parent_child<'a>(
+        &self,
+        state: &'a CursorState,
+        src: &'a Address,
+    ) -> Result<(&'a Node, usize), CursorError> {
+        let src = src.coerce(&state.graph_hash)?;
+        let Some(&node) = state.graph_hash.get(&src) else {
+            return Err(CursorError::InvalidAddress(src.clone()));
+        };
+        let Some(ref parent_addr) = node.parent_addr else {
+            // This node is a FNDEF/CLSDECL; cannot go up without going out first.
+            return Err(CursorError::InvalidMotion(*self));
+        };
+        let Some(&parent) = state
+            .graph_hash
+            .get(&parent_addr.coerce(&state.graph_hash)?)
+        else {
+            return Err(CursorError::InvalidAddress(parent_addr.clone()));
+        };
+
+        let (i, curr_node): (usize, &Node) = parent
+            .children
+            .iter()
+            .enumerate()
+            .filter(|(_, n)| n.addr == src)
+            .next()
+            .unwrap();
+        assert_eq!(curr_node, node);
+
+        Ok((parent, i))
+    }
+
     fn move_global<'a>(&'a self, state: &CursorState<'a>) -> anyhow::Result<Address, CursorError> {
         let src = &state.block_loc;
 
@@ -87,8 +120,7 @@ impl CursorDir {
                     check!(self._modifying_val() => dst[i] += 1 ; check!(dst[i] > 0 => dst[i] -= 1 ; continue));
                     let dst = Address::new(dst[0..(i + 1)].to_vec());
 
-                    // Ensure the validity of the returned address (i.e., if going right from the
-                    // rightmost block, return an InvalidMotion error, not a CoercionError).
+                    // Ensure the motion worked (i.e., not going right from the rightmost block).
                     let Ok(mut dst) = dst.coerce(&state.graph_hash) else {
                         return Err(CursorError::InvalidMotion(*self));
                     };
@@ -99,8 +131,8 @@ impl CursorDir {
                 return Err(CursorError::InvalidMotion(*self));
             }
             CursorDir::IN => {
-                // Essentially coerces a block to its root node
-                todo!()
+                let dst = src.coerce(&state.graph_hash)?;
+                Ok(dst)
             }
             _ => return Err(CursorError::InvalidMotion(*self)),
         }
@@ -110,16 +142,70 @@ impl CursorDir {
         &'cur self,
         state: &CursorState<'cur>,
     ) -> anyhow::Result<Address, CursorError> {
+        let src: &Address = &state.block_loc;
+
         // When going up and down, no coercion - check children for local blocks
         // Left and right involves finding the parent's next child
         // In and out involves directly finding parent and child and moving (up until global block)
         match self {
-            CursorDir::UP => todo!(),
-            CursorDir::DOWN => todo!(),
-            CursorDir::LEFT => todo!(),
-            CursorDir::RIGHT => todo!(),
-            CursorDir::OUT => todo!(),
-            CursorDir::IN => todo!(),
+            CursorDir::UP => {
+                let (parent, i) = self._find_parent_child(state, src)?;
+                // If this is the parent's root child, go out and coerce to the nearest node
+                if i < N_ROOT_CHILDREN[&format!("{:?}", parent.kind)] as usize {
+                    let dst = CursorDir::OUT.move_local(state)?;
+                    let dst = dst.coerce(&state.graph_hash)?;
+                    return Ok(dst);
+                }
+                // Return the parent's previous child -> this should be the one right above
+                Ok(parent.children[i - 1].addr.clone())
+            }
+            CursorDir::DOWN => {
+                let Some(&node) = state.graph_hash.get(&src.coerce(&state.graph_hash)?) else {
+                    return Err(CursorError::InvalidAddress(src.clone()));
+                };
+                if N_ROOT_CHILDREN.contains_key(&format!("{:?}", node.kind)[..]) {
+                    return Ok(node.children[0].addr.clone());
+                }
+
+                let (parent, i) = self._find_parent_child(state, src)?;
+                if i == parent.children.len() - 1 {
+                    return Err(CursorError::InvalidMotion(*self));
+                }
+
+                // Return the parent's next child -> this should be the one right below
+                // NOTE: XXX: Need the returned address always have the same length as `src`?
+                Ok(Address::new(
+                    parent.children[i + 1].addr.addr[..src.len()].to_vec(),
+                ))
+            }
+            CursorDir::LEFT => {
+                let (parent, i) = self._find_parent_child(state, src)?;
+                match parent.children[i].kind {
+                    NodeKind::CONDTLN => Ok(parent.children[i - 1].addr.clone()),
+                    _ => Err(CursorError::InvalidMotion(*self)),
+                }
+            }
+            CursorDir::RIGHT => {
+                let (parent, i) = self._find_parent_child(state, src)?;
+                match parent.children[i].kind {
+                    NodeKind::CONDTLY => Ok(parent.children[i + 1].addr.clone()),
+                    _ => Err(CursorError::InvalidMotion(*self)),
+                }
+            }
+            CursorDir::IN => {
+                // let mut src = &mut src.clone();
+                // src.addr.push(0);
+                // let Ok(_) = src.coerce(&state.graph_hash) else {
+                //     return Err(CursorError::InvalidMotion(*self));
+                // };
+                // Ok(src.clone())
+                self.move_global(state)
+            }
+            CursorDir::OUT => {
+                let mut src = &mut src.clone();
+                let _ = src.addr.pop();
+                Ok(src.clone())
+            }
         }
     }
 }
@@ -183,6 +269,7 @@ impl<'dag> CursorState<'dag> {
 mod tests {
     use crate::addr;
     use crate::digraph::address::{Address, Addressable};
+    use crate::digraph::parser::NodeKind;
     use crate::digraph::parser::Parser;
     use crate::digraph::state::{CursorError::InvalidMotion, CursorState};
 
@@ -196,7 +283,12 @@ mod tests {
             let mut state = CursorState::new(graph, graph.get_hash()).unwrap();
             let mut failed = false;
             $(
-                let dst = move_cursor!($dir).move_global(&state);
+                let coerced = state.block_loc.coerce(&state.graph_hash).expect("Coercion should work");
+                let dst = if coerced != state.block_loc && state.graph_hash.get(&coerced).expect("Retrieval should work").kind == NodeKind::FNDEF {
+                    move_cursor!($dir).move_global(&state)
+                } else {
+                    move_cursor!($dir).move_local(&state)
+                };
                 failed = failed || matches!(dst, Err($err(_)));
                 if let Ok(dst) = dst {
                     state.block_loc = dst;
@@ -211,7 +303,12 @@ mod tests {
             let graph = &nodes[..];
             let mut state = CursorState::new(graph, graph.get_hash()).unwrap();
             $(
-                let dst = move_cursor!($dir).move_global(&state).expect("motion should succeed");
+                let coerced = state.block_loc.coerce(&state.graph_hash).expect("Coercion should work");
+                let dst = if coerced != state.block_loc && state.graph_hash.get(&coerced).expect("Retrieval should work").kind == NodeKind::FNDEF {
+                    move_cursor!($dir).move_global(&state).expect("Motion should succeed")
+                } else {
+                    move_cursor!($dir).move_local(&state).expect("Motion should succeed")
+                };
                 state.block_loc = dst.clone();
             )+
             assert_eq!(dst, addr!($($id),+));
@@ -219,6 +316,9 @@ mod tests {
         // Cycles are sequences of motions that end up at the leftmost root (i.e., <0, 0>)
         ($($dir:tt),+ _in_ $src:ident ||) => {{
             move_cursor!($($dir),+ _in_ $src -> <0, 0>);
+        }};
+        ($($dir:tt),+ _in_ $src:ident ##) => {{
+            move_cursor!($($dir),+ _in_ $src -> <1, 0>);
         }};
         (U) => {$crate::digraph::state::CursorDir::UP};
         (D) => {$crate::digraph::state::CursorDir::DOWN};
@@ -230,31 +330,63 @@ mod tests {
 
     const SOURCE: &str = "define f of x\noutput x\ndefine f1 of x\noutput x\ndone define\ndefine \
                           f2 of x\noutput x\ndefine f21 of x\noutput x\ndone define\ndefine f22 of \
-                          x\noutput x\ndone define\ndone define\ndone define\ndefine h of x\noutput x plus 1\nif \
+                          x\noutput x\ndone define\ndone define\ndone define\ndefine g of x\noutput x plus 1\nif \
                           x equals 3\noutput x\ndone if\notherwise\noutput y\ndone otherwise\ndone \
                           define";
 
-    #[test]
-    fn global_invalid() {
-        move_cursor!(D, D _in_ SOURCE -> InvalidMotion);
-        move_cursor!(D, R, D, R, R _in_ SOURCE -> InvalidMotion);
-        move_cursor!(D, R, D, R, D _in_ SOURCE -> InvalidMotion);
-        move_cursor!(D, R, D, D _in_ SOURCE -> InvalidMotion);
+    mod global {
+        use super::*;
+
+        #[test]
+        fn global_invalid() {
+            move_cursor!(D, D _in_ SOURCE -> InvalidMotion);
+            move_cursor!(D, R, D, R, R _in_ SOURCE -> InvalidMotion);
+            move_cursor!(D, R, D, R, D _in_ SOURCE -> InvalidMotion);
+            move_cursor!(D, R, D, D _in_ SOURCE -> InvalidMotion);
+        }
+
+        #[test]
+        fn global_compound() {
+            move_cursor!(D, R _in_ SOURCE -> <0, 1, 1, 0>);
+            move_cursor!(D, R, D, R _in_ SOURCE -> <0, 1, 1, 1, 1>);
+            move_cursor!(D, R, D, R, L _in_ SOURCE -> <0, 1, 1, 1, 0>);
+        }
+
+        #[test]
+        fn global_cycles() {
+            move_cursor!(R, L _in_ SOURCE ||);
+            move_cursor!(D, U _in_ SOURCE ||);
+            move_cursor!(D, R, U _in_ SOURCE ||);
+            move_cursor!(D, R, L, R, U _in_ SOURCE ||);
+            move_cursor!(D, R, D, R, U, L, U _in_ SOURCE ||);
+        }
     }
 
-    #[test]
-    fn global_compound() {
-        move_cursor!(D, R _in_ SOURCE -> <0, 1, 1, 0>);
-        move_cursor!(D, R, D, R _in_ SOURCE -> <0, 1, 1, 1, 1>);
-        move_cursor!(D, R, D, R, L _in_ SOURCE -> <0, 1, 1, 1, 0>);
-    }
+    mod local {
+        use super::*;
 
-    #[test]
-    fn global_cycles() {
-        move_cursor!(R, L _in_ SOURCE ||);
-        move_cursor!(D, U _in_ SOURCE ||);
-        move_cursor!(D, R, U _in_ SOURCE ||);
-        move_cursor!(D, R, L, R, U _in_ SOURCE ||);
-        move_cursor!(D, R, D, R, U, L, U _in_ SOURCE ||);
+        #[test]
+        fn local_linear() {
+            move_cursor!(R, I _in_ SOURCE -> <1, 0, 0>);
+            move_cursor!(R, I, D _in_ SOURCE -> <1, 0, 1>);
+            move_cursor!(R, I, D, D _in_ SOURCE -> <1, 0, 2>);
+        }
+
+        #[test]
+        fn local_depth() {
+            move_cursor!(R, I, D, D, I _in_ SOURCE -> <1, 0, 2, 0, 0>);
+            move_cursor!(R, I, D, D, I, D _in_ SOURCE -> <1, 0, 2, 1, 0, 0>);
+            move_cursor!(R, I, D, D, I, D, D _in_ SOURCE -> <1, 0, 2, 1, 0, 1>);
+            move_cursor!(R, I, D, D, I, D, R _in_ SOURCE -> <1, 0, 2, 1, 1, 0>);
+            move_cursor!(R, I, D, D, I, D, D, D _in_ SOURCE -> InvalidMotion);
+        }
+
+        #[test]
+        fn local_cycles() {
+            move_cursor!(R, I, D, U, O _in_ SOURCE ##);
+            move_cursor!(R, I, D, D, U, U, O _in_ SOURCE ##);
+            move_cursor!(R, I, D, D, U, U, O _in_ SOURCE ##);
+            move_cursor!(R, I, D, D, O _in_ SOURCE ##);
+        }
     }
 }
