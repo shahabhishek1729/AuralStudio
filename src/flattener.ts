cb730eeed0a32fe61b90f968f48da9c07a6f5edc
Abhishek Shah <theartofcoding2048@gmail.com>
2024-08-11 20:18:07 -0400

    Built generator for flattener

--- src/flattener.ts
+++ src/flattener.ts
@@ -1,10 +1,10 @@
 export interface node {
   id: number;
   children: node[];
-  blocks: block[];
+  pieces: piece[];
 }
 
-export interface block {
+export interface piece {
   kind: string;
   name?: string | number | boolean;
   line: number;
@@ -18,23 +18,24 @@ export interface flatnode {
 
 type parent_id = number | null;
 
-function flatten(
-  data: node[],
-  level: number = 1,
-  parent: parent_id = null,
-  result: flatnode[] = []
-): flatnode[] {
+/**
+ * Flattens a nested, reecursive array of nodes into an arary with depth=1 
+ * @param data The nested/recursive array of nodes 
+ * @param level The depth of a given flatnode (should always be initialized to 1, will be changed 
+ *              in recursive calls within the function)
+ * @param parent The id of a flatnode's parent (should always be initialized to null, will be changed
+ *				in recursive calls within the function)
+ */
+export function* flatten(data: node[], level: number = 1, parent: parent_id = null): IterableIterator<flatnode> {
   for (let i = 0; i < data.length; i++) {
-    result.push({
+    yield {
       id: data[i].id,
       level: level,
       parent: parent,
-    });
+    };
     if (data[i].children.length > 0) {
-      result.concat(flatten(data[i].children, level + 1, data[i].id, result));
+      yield* flatten(data[i].children, level + 1, data[i].id);
     }
   }
-  return result;
 }
 
-export default flatten;
