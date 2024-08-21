0e176571e4f6cb1924e56740784c96e65a22aa19
Abhishek Shah <theartofcoding2048@gmail.com>
2024-08-21 19:18:22 -0400

    Removed non-snake crate name warning

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![allow(non_snake_case)]
 
 pub mod digraph;
 pub mod error;
