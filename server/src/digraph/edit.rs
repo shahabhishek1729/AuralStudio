0e1165365c29d798243cbda3825269cafd6ce1bf
Abhishek Shah <theartofcoding2048@gmail.com>
2024-08-27 13:56:55 -0400

    Added implementation and tests for getting insert loc

--- server/src/digraph/edit.rs
+++ server/src/digraph/edit.rs
@@ -3,28 +3,30 @@ use crate::digraph::address::{Address, Addressable};
 use crate::digraph::util::*;
 use crate::prelude::CursorError;
 
-pub(crate) struct Editor {
-    state: CursorState,
+pub(crate) struct Editor<'a> {
+    state: &'a CursorState,
     insert_loc: Option<Address>,
 }
 
-impl Editor {
-    pub(crate) fn new(state: CursorState) -> Result<Self, CursorError> {
+impl<'a> Editor<'a> {
+    pub(crate) fn new(state: &'a CursorState) -> Result<Self, CursorError> {
         let Some(&curr_node) = state.graph.get_hash().get(&state.node_loc) else {
-            return Err(CursorError::AddrNotFound(state.node_loc));
+            return Err(CursorError::AddrNotFound(state.node_loc.clone()));
         };
 
         let insert_loc = match curr_node.kind {
-            _ if state._at_node() => Some({
-                // If we're on a node, just make a new node below and as the new `insert_loc`
-                let Some(next_addr) = curr_node.addr.next() else {
-                    return Err(CursorError::EmptyAddr);
-                };
-                next_addr
-            }),
-            super::parser::NodeKind::FNDEF => Some({
+            super::parser::NodeKind::CONDTL if state._at_node() => {
+                return Err(CursorError::InsertConditional);
+            }
+            super::parser::NodeKind::FNDEF if !state._at_node() => {
                 // On a function block, so we need to know how many global children this block has.
-                let num_blocks = _filter_children(curr_node).collect::<Vec<_>>().len();
+                let num_blocks = curr_node
+                    .children
+                    .iter()
+                    .filter(|c| GLOBAL_BLOCKS.contains(&c.kind))
+                    .collect::<Vec<_>>()
+                    .len();
+                dbg!(&num_blocks);
 
                 if num_blocks == 0 {
                     // No global children, append <1(new level), 0(first child in that level)>
@@ -38,21 +40,60 @@ impl Editor {
                     };
                     next_addr.join(&[num_blocks])
                 }
-            }),
-            super::parser::NodeKind::CONDTL => {
-                return Err(CursorError::InsertConditional);
             }
-            _ => None,
+            _ => {
+                // If we're on a node, just make a new node below and as the new `insert_loc`
+                let Some(next_addr) = curr_node.addr.next() else {
+                    return Err(CursorError::EmptyAddr);
+                };
+                next_addr
+            }
         };
 
-        Ok(Self { state, insert_loc })
+        Ok(Self {
+            state,
+            insert_loc: Some(insert_loc),
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::addr;
+    use crate::digraph::address::Addressable;
+    use crate::digraph::parser::Parser;
+    use crate::digraph::state::ADMode;
+
+    const SOURCE: &'static str = "define f of x\noutput x\ndefine f1 of x\noutput x\ndone define\ndefine \
+                          f2 of x\noutput x\ndefine f21 of x\noutput x\ndone define\ndefine f22 of \
+                          x\noutput x\ndone define\ndone define\ndone define\ndefine g of x\noutput x plus 1\nif \
+                          x equals 3\noutput x\ndone if\notherwise\noutput y\ndone otherwise\ndone \
+                          define";
+
+    macro_rules! insert {
+        // Move sequences that result in an attempted move "off the graph" should return errors
+        (@ <$($id:literal),+> _in_ $src:ident -> <$($new_id:literal),+>) => {{
+            let mut parser = Parser::new(String::from($src)).unwrap();
+            let mut nodes = parser.parse().unwrap();
+            (&mut nodes[..]).fill_addr();
+
+            let block_loc = addr!($($id),+);
+            let node_loc = block_loc.coerce(&nodes.get_hash()).expect("Node coercion should work");
+            let mut state = CursorState {
+                block_loc,
+                node_loc,
+                mode: ADMode::VIEW,
+                insert_at: None,
+                graph: nodes.to_vec(),
+            };
+            let editor = Editor::new(&state);
+            assert_eq!(editor.expect("Coercion should work").insert_loc, Some(addr!($($new_id),+)));
+        }};
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(1 + 1, 2);
+    fn fn_insert_loc() {
+        insert!(@ <0, 0> _in_ SOURCE -> <0, 1, 2>)
     }
 }
