41735d89d575de88e3476f15e0faf78d6b2b5ff8
Abhishek Shah <theartofcoding2048@gmail.com>
2024-09-15 17:01:19 -0400

    Insert placeholders for function calls and sync graph

--- server/src/digraph/edit.rs
+++ server/src/digraph/edit.rs
@@ -1,3 +1,4 @@
+use super::address::Address;
 use super::parser::{Node, NodeKind, Piece};
 use super::state::{ADMode, CursorState};
 use crate::digraph::address::Addressable;
@@ -11,6 +12,7 @@ impl CursorState {
             return Err(CursorError::AddrNotFound(self.node_loc.clone()));
         };
         let parent_addr = curr_node.parent_addr.clone();
+        let curr_addr = curr_node.addr.clone();
 
         let (insert_loc, expecting) = match curr_node.kind {
             super::parser::NodeKind::CONDTL if self._at_node() => {
@@ -25,21 +27,22 @@ impl CursorState {
                     .collect::<Vec<_>>()
                     .len();
 
-                (
-                    if num_blocks == 0 {
-                        // No global children, append:
-                        // <1 (new level), 0 (first child in that level), 0 (root of the block)>
-                        self.block_loc.join(&[1, 0, 0])
-                    } else {
-                        // There are global children -> precondition: curr_addr.len() % 2 == 0
-                        let Some(next_addr) = self.block_loc.next() else {
-                            return Err(CursorError::EmptyAddr);
-                        };
-                        // Append <num_blocks (the index of the new node), 0 (root of the block)>
-                        next_addr.join(&[num_blocks, 0])
-                    },
-                    Some(NodeKind::FNDEF), // The next node must be a function definition
-                )
+                let insert_loc_ = if num_blocks == 0 {
+                    // No global children, append:
+                    // <1 (new level), 0 (first child in that level), 0 (root of the block)>
+                    self.block_loc.join(&[1, 0, 0])
+                } else {
+                    // There are global children -> precondition: curr_addr.len() % 2 == 0
+                    let Some(next_addr) = self.block_loc.next() else {
+                        return Err(CursorError::EmptyAddr);
+                    };
+                    // Append <num_blocks (the index of the new node), 0 (root of the block)>
+                    next_addr.join(&[num_blocks, 0])
+                };
+
+                self._insert_fn(&insert_loc_, curr_addr)?;
+
+                (insert_loc_, Some(NodeKind::FNDEF))
             }
             _ => {
                 // If we're on a node, just make a new node below and as the new `insert_loc`
@@ -49,24 +52,8 @@ impl CursorState {
 
                 // NOTE: Subtraction is safe because this address must be >= 1 (parent is 0)
                 let child_ix = next_addr.last().expect("child address cannot be empty") - 1;
-                let hash_ = self.graph.get_hash_mut();
 
-                let Some(parent_node) = hash_.get(&parent_addr) else {
-                    return Err(CursorError::ParentNotFound(parent_addr));
-                };
-
-                // SAFETY: The parent node must be valid because this node is not a root function
-                // (which would be in the FNDEF branch) so `parent_addr` must point to a valid node.
-                let parent_node = unsafe { &mut **parent_node };
-                let new_node = Node {
-                    line: 0, // TODO: How to increment all subsequent line numbers efficiently?
-                    children: vec![],
-                    kind: NodeKind::PENDING,
-                    pieces: vec![Piece::PENDING],
-                    addr: next_addr.clone(),
-                    parent_addr,
-                };
-                parent_node.children.insert(child_ix, new_node);
+                self._insert_other(&next_addr, parent_addr, child_ix)?;
 
                 (next_addr, None) // There are a number of possible nodes that could follow
             }
@@ -74,6 +61,56 @@ impl CursorState {
 
         self.mode = ADMode::EDIT(expecting);
         self.block_loc = insert_loc;
+
+        // Re-sync up the graph with the new additions of nodes
+        (&mut self.graph[..]).fill_addr();
+        Ok(())
+    }
+
+    // Update graph with a new function (@ `at_addr`) as a child of the function @ `from_addr`.
+    fn _insert_fn(&mut self, at_addr: &Address, from_addr: Address) -> Result<(), CursorError> {
+        let hash_ = self.graph.get_hash_mut();
+        let Some(parent_node) = hash_.get(&from_addr) else {
+            return Err(CursorError::ParentNotFound(from_addr));
+        };
+        // SAFETY: The parent node must be valid because this node is not a root function
+        // (which would be in the FNDEF branch) so `parent_addr` must point to a valid node.
+        let parent_node = unsafe { &mut **parent_node };
+        let new_node = Node {
+            line: 0, // TODO: How to increment all subsequent line numbers efficiently?
+            children: vec![],
+            kind: NodeKind::FNDEF,
+            pieces: vec![Piece::PENDING],
+            addr: at_addr.clone(),
+            parent_addr: from_addr,
+        };
+        parent_node.children.push(new_node);
+        Ok(())
+    }
+
+    // Update graph with a new node (@ `at_addr`) as the `index`th child of the node @ `from_addr`.
+    fn _insert_other(
+        &mut self,
+        at_addr: &Address,
+        from_addr: Address,
+        index: usize,
+    ) -> Result<(), CursorError> {
+        let hash_ = self.graph.get_hash_mut();
+        let Some(parent_node) = hash_.get(&from_addr) else {
+            return Err(CursorError::ParentNotFound(from_addr));
+        };
+        // SAFETY: The parent node must be valid because this node is not a root function
+        // (which would be in the FNDEF branch) so `parent_addr` must point to a valid node.
+        let parent_node = unsafe { &mut **parent_node };
+        let new_node = Node {
+            line: 0, // TODO: How to increment all subsequent line numbers efficiently?
+            children: vec![],
+            kind: NodeKind::PENDING,
+            pieces: vec![Piece::PENDING],
+            addr: at_addr.clone(),
+            parent_addr: from_addr,
+        };
+        parent_node.children.insert(index, new_node);
         Ok(())
     }
 }
